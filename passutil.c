3b73c60bdc92ee95f9600d79f212c3c7b2eb6bd6
Gleb Salmanov <nullcaller@gmail.com>
2025-01-03 02:13:41 +0300

    Initialize repository

---
+++ passutil.c
@@ -0,0 +1,20 @@
+/* passutil
+ * 
+ * A utility to generate, securely store and memorize passwords.
+ * For maximum security, should be used on a standalone machine that never connects to the internet.
+ * The machine in question should feature full drive encryption to avoid tampering.
+ * 
+ * Copyright (c) 2025 Gleb Salmanov
+ * All rights reserved.
+ */
+
+#include<unistd.h>
+#include<stdio.h>
+
+int main(int argc, char* argv[]) {
+	int opt;
+
+	while() {
+		
+	}
+}
