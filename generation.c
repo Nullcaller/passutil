2804c60a4d8dec6de140f44d0adcac65a3e3ed63
Глеб Сальманов <nullcaller@gmail.com>
2025-01-03 21:05:15 +0300

    Add shuffle key generation

---
+++ generation.c
@@ -0,0 +1,22 @@
+#include<sodium.h>
+#include<string.h>
+
+void generate_shuffle_key(char** key_var, char* key_format) {
+	unsigned int len = strlen(key_format);
+	char* key_remainder = malloc(len+1);
+	memcpy(key_remainder, key_format, len+1);
+	char* key = malloc(len);
+
+	unsigned int it = 0;
+	unsigned int pick;
+	while(key_format[it] != '\0') {
+		pick = randombytes_uniform(len);
+		key[it] = key_remainder[pick];
+		strncpy(key_remainder+pick, key_remainder+pick+1, len-pick);
+		len--;
+		it++;
+	}
+
+	free(key_remainder);
+	*key_var = key;
+}
