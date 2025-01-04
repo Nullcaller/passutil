a2ea7d35bf96ade22b125b6bfad93d7dbf06d0e9
nullcaller <nullcaller@gmail.com>
2025-01-04 14:46:39 +0300

    Add passwod generation functions

--- generation.c
+++ generation.c
@@ -3,6 +3,8 @@
 
 #include "generation.h"
 
+#include "storage.h"
+
 void generate_shuffle_key(char** key_var, char* key_format) {
 	unsigned int len = strlen(key_format);
 	char* key_remainder = malloc(len+1);
@@ -27,4 +29,22 @@ unsigned char* generate_password_bytes(unsigned int length) {
 	unsigned char* bytes = malloc(sizeof(unsigned char)*length);
 	randombytes_buf(bytes, length);
 	return bytes;
+}
+
+Password* generate_password(Store* store, char* format, unsigned int length) {
+	Password* password = construct_password();
+
+	unsigned int length_bytes = length*(sizeof(unsigned long long)/sizeof(char));
+	unsigned char* plain_password_bytes = generate_password_bytes(length_bytes);
+	
+	write(store, password, plain_password_bytes, length_bytes, format, length);
+	
+	free(plain_password_bytes);
+	return password;
+}
+
+void generate_password_and_append(Store* store, char* identifier, char* format, unsigned int length) {
+	Password* password = generate_password(store, format, length);
+
+	append_password(store, password, identifier);
 }
--- generation.h
+++ generation.h
@@ -1,3 +1,9 @@
+#include "storage.h"
+
 void generate_shuffle_key(char** key_var, char* key_format);
 
 unsigned char* generate_password_bytes(unsigned int length);
+
+Password* generate_password(Store* store, unsigned int length);
+
+void generate_password_and_append(Store* store, char* identifier, char* format, unsigned int length);
