e74170b0af30d725670856bb66d167813a2f38fc
nullcaller <nullcaller@gmail.com>
2025-01-04 14:51:06 +0300

    Add pre-defined formats

---
+++ formats.h
@@ -0,0 +1,7 @@
+#define FORMAT_AZaz09 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+#define FORMAT_AZaz09_symb = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!\"@#$%'()^&*-=_+,./\\:;<>?[]{}`|~"
+
+#define FORMAT_AZaz09_sp = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 "
+
+#define FORMAT_AZaz09_symb_sp = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!\"@#$%'()^&*-=_+,./\\:;<>?[]{}`|~ "
