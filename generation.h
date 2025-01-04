53321b85fbabf187dc76f53b19d488ce005fd8c3
nullcaller <nullcaller@gmail.com>
2025-01-04 14:47:21 +0300

    Fix header

--- generation.h
+++ generation.h
@@ -4,6 +4,6 @@ void generate_shuffle_key(char** key_var, char* key_format);
 
 unsigned char* generate_password_bytes(unsigned int length);
 
-Password* generate_password(Store* store, unsigned int length);
+Password* generate_password(Store* store, char* format, unsigned int length);
 
 void generate_password_and_append(Store* store, char* identifier, char* format, unsigned int length);
