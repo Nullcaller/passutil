d3cf0088c1021c3e59dff2fa992884a35c4be3b1
nullcaller <nullcaller@gmail.com>
2025-01-04 04:41:18 +0300

    Make util encrypt/decrypt use unsigned chars

--- util.c
+++ util.c
@@ -13,7 +13,7 @@ char* strcpymalloc(char* string) {
 	return new_str;
 }
 
-char* encrypt(char* cipher, unsigned char* bytes, unsigned int length, char* shuffled_key, char* shuffle_key, char* shuffle_key_format) {
+unsigned char* encrypt(unsigned int* result_length, char* cipher, unsigned char* bytes, unsigned int length, char* shuffled_key, char* shuffle_key, char* shuffle_key_format) {
 	char* key = unshuffle(shuffled_key, shuffle_key, shuffle_key_format);
 	unsigned char* key_digest;
 	unsigned char* encrypted_bytes;
@@ -25,7 +25,8 @@ char* encrypt(char* cipher, unsigned char* bytes, unsigned int length, char* shu
 		free(key);
 		AES_KEY aes_key;
 		AES_set_encrypt_key(key_digest, 256, &aes_key);
-		encrypted_bytes = malloc(sizeof(unsigned char)*(length/AES_BLOCK_SIZE+1)*AES_BLOCK_SIZE);
+		*result_length = sizeof(unsigned char)*(length/AES_BLOCK_SIZE+1)*AES_BLOCK_SIZE;
+		encrypted_bytes = malloc(*result_length);
 		iv = malloc(sizeof(unsigned char)*AES_BLOCK_SIZE);
 		memset(iv, 0, AES_BLOCK_SIZE);
 		AES_cbc_encrypt(bytes, encrypted_bytes, length, &aes_key, iv, AES_ENCRYPT);
@@ -37,7 +38,7 @@ char* encrypt(char* cipher, unsigned char* bytes, unsigned int length, char* shu
 	}
 }
 
-char* decrypt(char* cipher, unsigned char* encrypted_bytes, unsigned int length, char* shuffled_key, char* shuffle_key, char* shuffle_key_format) {
+unsigned char* decrypt(char* cipher, unsigned char* encrypted_bytes, unsigned int length, char* shuffled_key, char* shuffle_key, char* shuffle_key_format) {
 	char* key = unshuffle(shuffled_key, shuffle_key, shuffle_key_format);
 	unsigned char* key_digest;
 	unsigned char* bytes;
