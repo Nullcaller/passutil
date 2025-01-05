cfcc5642fe887e9e717c78dfd0bf256807377ec9
nullcaller <nullcaller@gmail.com>
2025-01-06 01:36:03 +0300

    Remove copyright notice from passutil.c

--- passutil.c
+++ passutil.c
@@ -4,8 +4,7 @@
  * For maximum security, should be used on a standalone machine that never connects to the internet.
  * The machine in question should feature full drive encryption to avoid tampering.
  * 
- * Copyright (c) 2025 Gleb Salmanov
- * All rights reserved.
+ * by Gleb Salmanov
  */
 
 #include<stdio.h>
