a04d4db07715a6f53f2667e955a9fc08cb58dc64
nullcaller <nullcaller@gmail.com>
2025-01-06 12:39:09 +0300

    Add pseudoshell yes-no prompter

--- pseudoshell.c
+++ pseudoshell.c
@@ -1,2 +1,38 @@
+#include<stdio.h>
+#include<stdbool.h>
+
 #include "pseudoshell.h"
 
+int _present_yesno_prompt(char* prompt) {
+	printf(prompt);
+	printf(" (Y/n) ");
+
+	char* buf = malloc(sizeof(char)*PSEUDOSHELL_BUFFER_SIZE);
+	char* scanfstr = malloc(sizeof(char)*PSEUDOSHELL_BUFFER_SIZE);
+
+	sprintf(scanfstr, "%%%d[^\\n]", PSEUDOSHELL_BUFFER_SIZE-1);
+	if(scanf(scanfstr, buf) <= 0) {
+		free(buf);
+		free(scanfstr);
+		return -1;
+	}
+	free(scanfstr);
+
+	if(buf[0] == 'Y' || buf[0] == 'y') {
+		free(buf);
+		return 1;
+	} else if(buf[0] == 'N' || buf[0] == 'n') {
+		free(buf);
+		return 0;
+	} else {
+		free(buf);
+		return -2;
+	}
+}
+
+bool present_yesno_prompt(char* prompt) {
+	int result = -1;
+	while(result < 0)
+		_present_yesno_prompt(prompt);
+	return result;
+}
--- pseudoshell.h
+++ pseudoshell.h
@@ -1,6 +1,10 @@
+#include<stdbool.h>
+
 #ifndef PSEUDOSHELL_H
 #define PSEUDOSHELL_H
 
+#define PSEUDOSHELL_BUFFER_SIZE 4096
 
+bool present_yesno_prompt(char* prompt);
 
 #endif
