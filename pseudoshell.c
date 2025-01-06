e7a14871ebe798149e1384cb6bf75f3aef6b0a3e
nullcaller <nullcaller@gmail.com>
2025-01-06 13:45:47 +0300

    Fix pseudoshell yes-no prompter

--- pseudoshell.c
+++ pseudoshell.c
@@ -3,36 +3,50 @@
 
 #include "pseudoshell.h"
 
+#include "util.h"
+
+int _getstr(char** str, unsigned int piece_length) {
+	char* _str = NULL;
+	unsigned int str_allocated_length = 0;
+	unsigned int str_length = 0;
+
+	char character;
+	while((character = getchar()) != '\n' && character != '\r' && character != EOF)
+		_str = strappendcharrealloc(_str, &str_allocated_length, &str_length, piece_length, character);
+
+	if(_str != NULL)
+		_str = strtrimrealloc(_str, &str_allocated_length);
+	else {
+		_str = malloc(sizeof(char));
+		_str[0] = '\0';
+		*str = _str;
+		return 0;
+	}
+
+	*str = _str;
+	return str_length+1;
+}
+
 int _present_yesno_prompt(char* prompt) {
 	printf(prompt);
 	printf(" (Y/n) ");
 
-	char* buf = malloc(sizeof(char)*PSEUDOSHELL_BUFFER_SIZE);
-	char* scanfstr = malloc(sizeof(char)*PSEUDOSHELL_BUFFER_SIZE);
+	char* str;
 
-	sprintf(scanfstr, "%%%d[^\\n]", PSEUDOSHELL_BUFFER_SIZE-1);
-	if(scanf(scanfstr, buf) <= 0) {
-		free(buf);
-		free(scanfstr);
+	if(_getstr(&str, PSEUDOSHELL_BUFFER_SIZE) <= 0)
 		return -1;
-	}
-	free(scanfstr);
-
-	if(buf[0] == 'Y' || buf[0] == 'y') {
-		free(buf);
+		
+	if(str[0] == 'Y' || str[0] == 'y')
 		return 1;
-	} else if(buf[0] == 'N' || buf[0] == 'n') {
-		free(buf);
+	else if(str[0] == 'N' || str[0] == 'n')
 		return 0;
-	} else {
-		free(buf);
+	else
 		return -2;
-	}
 }
 
 bool present_yesno_prompt(char* prompt) {
 	int result = -1;
 	while(result < 0)
-		_present_yesno_prompt(prompt);
+		result = _present_yesno_prompt(prompt);
 	return result;
 }
